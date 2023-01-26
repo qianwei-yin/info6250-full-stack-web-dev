6f3bc945f89360126be90d085b415b1a876393d0
qianwei-yin <qianweiyin22@gmail.com>
2023-01-25 16:07:56 -0800

    Edit function to compare two words

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -1,10 +1,37 @@
-"use strict";
+'use strict';
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY
+function compare(word, guess) {
+	// DO NOT MODIFY
 
-/* YOU MAY MODIFY THE LINES BELOW */
+	/* YOU MAY MODIFY THE LINES BELOW */
+	const word1 = lowerSort(word);
+	const word2 = lowerSort(guess);
 
-  return 0; // this line is wrong
+	const wordLength = word1.length;
+
+	let [index1, index2, sameNum] = [0, 0, 0];
+	while (index1 < wordLength && index2 < wordLength) {
+		if (word1[index1].localeCompare(word2[index2]) === 0) {
+			sameNum += 1;
+			index1 += 1;
+			index2 += 1;
+		} else if (word1[index1].localeCompare(word2[index2]) < 0) {
+			index1 += 1;
+		} else {
+			index2 += 1;
+		}
+	}
+
+	return sameNum;
+}
+
+function lowerSort(word) {
+	return word
+		.toLowerCase()
+		.split('')
+		.sort((a, b) => {
+			return a.localeCompare(b);
+		});
 }
